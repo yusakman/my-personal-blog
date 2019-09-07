fda1608eb2b6f6bc996e9c4ab85b6159cc26d76f
yusakman <yusakmanullang93@gmail.com>
2019-09-08 02:33:16 +0700

    Fix an error

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import { Header } from 'components';
 import { Layout, Container } from 'layouts';
-import logo from '../../static/logo/profile.jpg';
+import logo from '../../static/logo/Profile.jpg';
 
 const About = center => (
   <Layout>
